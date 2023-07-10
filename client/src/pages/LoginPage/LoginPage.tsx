6f174cc61cb46b66fb55b4831c13ae379352a728
k22y22 <skehehdanfdldk@gmail.com>
2023-07-10 17:41:20 +0900

    fix/에러사항 수정

--- client/src/pages/LoginPage/LoginPage.tsx
+++ client/src/pages/LoginPage/LoginPage.tsx
@@ -49,7 +49,7 @@ const LoginPage: React.FC = () => {
     <div className="flex flex-col items-center justify-center">
       <form>
         <div className="text-center text-xl">LOGO</div>
-        <div className="mb-6 border-b-2 border-darkGray text-center text-2xl">로그인</div>
+        <div className="mb-6 border-b-2 border-mainblack text-center text-2xl">로그인</div>
 
         <div className="mb-4">
           <input
@@ -58,7 +58,7 @@ const LoginPage: React.FC = () => {
             placeholder="이메일 입력"
             value={userEmail}
             onChange={(e) => setUserEmail(e.target.value)}
-            className="w-full border-2 border-zinc-300 px-1 py-2 focus:border-b-2 focus:border-darkGray focus:outline-none"
+            className="w-full border-2 border-zinc-300 px-1 py-2 focus:border-b-2 focus:border-mainblack focus:outline-none"
           />
         </div>
 
@@ -69,21 +69,21 @@ const LoginPage: React.FC = () => {
             placeholder="비밀번호 입력"
             value={userPassword}
             onChange={(e) => setUserPassword(e.target.value)}
-            className="w-full border-2 border-zinc-300 px-1 py-2 focus:border-b-2 focus:border-darkGray focus:outline-none"
+            className="w-full border-2 border-zinc-300 px-1 py-2 focus:border-b-2 focus:border-mainblack focus:outline-none"
           />
         </div>
 
         {showInputError && <p className="mb-2 text-red-500">이메일과 비밀번호를 입력해주세요.</p>}
 
         <button
-          className="w-full bg-Gray px-1 py-2 text-white hover:bg-darkGray"
+          className="w-full bg-maindarkgray px-1 py-2 text-white hover:bg-mainblack"
           type="submit"
           onClick={(e) => handleLogin(e)}
         >
           로그인
         </button>
 
-        <button className="mt-2 w-full bg-darkGray px-1 py-2 text-white">
+        <button className="mt-2 w-full bg-mainblack px-1 py-2 text-white">
           <Link to="/signup"> 아직 계정이 없으신가요? </Link>
         </button>
         <SocialLogin />
