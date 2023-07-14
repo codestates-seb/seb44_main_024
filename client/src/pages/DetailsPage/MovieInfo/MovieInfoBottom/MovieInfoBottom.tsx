da8917565f4da083c38e9b3dd116b78ab5e9f9f4
dongbin420 <dongbin420@gmail.com>
2023-07-15 02:05:35 +0900

    스타일링 개선, 스크롤 효과

--- client/src/pages/DetailsPage/MovieInfo/MovieInfoBottom/MovieInfoBottom.tsx
+++ client/src/pages/DetailsPage/MovieInfo/MovieInfoBottom/MovieInfoBottom.tsx
@@ -3,13 +3,18 @@ import { selectMovieDetails } from '../../../../redux-toolkit/slices/movieDetail
 
 const MovieInfoBottom = () => {
   const movieDetail = useAppSelector(selectMovieDetails);
+
   return (
     <div>
-      <p className="mb-2">갤러리</p>
+      <p className="mb-4 text-2xl font-bold">갤러리</p>
       <div className="flex justify-between">
         {movieDetail?.movie.stills.map((still, index) => (
           <div key={index} className="w-[300px]">
-            <img className="h-full w-full" src={still} alt="still"></img>
+            <img
+              className="h-full w-full duration-300 ease-in-out hover:scale-110"
+              src={still}
+              alt="still"
+            ></img>
           </div>
         ))}
       </div>
