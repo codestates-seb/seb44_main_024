2a520af7506f20de9ff2d2c8dab6085268bfa038
k22y22 <skehehdanfdldk@gmail.com>
2023-07-24 17:35:38 +0900

    refactor: 리다이렉트 url 수정

--- client/src/constants/constants.ts
+++ client/src/constants/constants.ts
@@ -5,6 +5,6 @@ export const pwValidationRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z
 
 export const REST_API_KEY = 'c53bc747113925c65fbd2c3ff50ee43a';
 
-export const REDIRECT_URI = 'http://localhost:5173/redirect';
+export const REDIRECT_URI = 'http://code-main-24.s3-website.ap-northeast-2.amazonaws.com/redirect';
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
